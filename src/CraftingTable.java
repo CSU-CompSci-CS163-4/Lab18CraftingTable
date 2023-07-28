1a704b158c8ea00847c5463072c35875acfff88a
Emilie Beck <98420041+themilkprince@users.noreply.github.com>
2023-07-28 13:56:02 -0600

    Create CraftingTable.java

---
+++ src/CraftingTable.java
@@ -0,0 +1,25 @@
+public class CraftingTable {
+        /**
+         * This function should put the item in the table at the provided row and column
+         *
+         * @param  table 2D array you are putting the item in
+         * @param  item the name of the item you are putting in the table
+         * @param  row row index
+         * @param  column column index
+         * @return      the updated table
+         */
+        public String[][] addItem(String[][] table, String item, int row, int column){
+            return new String[0][0];
+        }
+        /**
+         * This function should remove the first instance of the item it is looking for and then stop
+         *
+         * @param  table 2D array you are searching
+         * @param  item the name of the item you are removing
+         * @return      the updated table
+         */
+        public String[][] removeItem(String[][] table, String item){
+            return new String[0][0];
+        }
+
+}
